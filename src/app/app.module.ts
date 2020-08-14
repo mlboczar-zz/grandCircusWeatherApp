570aac85a2cb6ad8f55922618e0311eb9cf0d9d3
Megan Boczar <mlboczar@gmail.com>
2020-08-13 20:40:01 -0400

    App Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './header/header.component';
+import { ResultsComponent } from './results/results.component';
+import { FormComponent } from './form/form.component';
+
+import { CommonModule } from '@angular/common';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
+  imports:      [ 
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    FormsModule , 
+    HttpClientModule,
+    CommonModule,
+    RouterModule.forRoot(
+      [
+        {path:'',component:FormComponent}
+      ])
+    ],
+  declarations: [ AppComponent, HeaderComponent, ResultsComponent, FormComponent],
+  bootstrap:    [ AppComponent ],
+  entryComponents : [FormComponent]
 })
 export class AppModule { }
